eff58d9a4b2fdb235390de79a98656697b69dcfe
Wildson <79426222+wildson-lab@users.noreply.github.com>
2024-05-13 17:07:13 -0300

    Implementado código para o Arduino escravo.

--- src/Arduino_Controlador.cpp
+++ src/Arduino_Controlador.cpp
@@ -8,10 +8,10 @@ e envio dos dados para um Concentrador.
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
+// Endereçamento de pinos
 #define PINO_SENSOR_TENSAO A0
 #define PINO_SENSOR_TEMPERATURA A1
 #define PINO_SENSOR_CORRENTE A2
-
 #define PINO_FOTOSENSOR 2
 #define PINO_INTERRUPT_SENSORES 3
 #define PINO_TELA_D4 4
@@ -22,38 +22,84 @@ e envio dos dados para um Concentrador.
 #define PINO_TELA_RS 9
 #define PINO_LED 13
 
+
+// Variáveis globais
 double tensao;
 double corrente;
 double temperatura;
 bool estado_foto;
 
 
+// Instâncias de bibliotecas
 LiquidCrystal lcd(PINO_TELA_RS, PINO_TELA_EN, PINO_TELA_D4, PINO_TELA_D5, PINO_TELA_D6, PINO_TELA_D7);
 
 
-void ISR_Sensores(){
-  digitalWrite(PINO_LED, digitalRead(PINO_INTERRUPT_SENSORES));
+// Protótipos de Funções
+
+double leitura_tensao();
+double leitura_corrente();
+double leitura_temperatura();
+void ISR_Sensores();
+void mostra_dados();
+void envia_dados();
+
+
+void setup() {
+  pinMode(PINO_LED, OUTPUT);
+  pinMode(PINO_FOTOSENSOR, INPUT);
+  pinMode(PINO_INTERRUPT_SENSORES, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PINO_INTERRUPT_SENSORES), ISR_Sensores, CHANGE);
+  Serial.begin(9600);
+  lcd.begin(16, 4);
 }
 
 
+void loop(){
+  tensao = leitura_tensao();
+  temperatura = leitura_temperatura();
+  corrente = leitura_corrente();
+  estado_foto = !digitalRead(PINO_FOTOSENSOR);
+
+  mostra_dados();
+  envia_dados();
+  delay(3000);
+}
+
+
+/// @brief Realiza leitura de tensão obtida pelo sensor
+/// @return Um double, com o valor da tensão (em Volts)
 double leitura_tensao(){
   double valor_bruto = analogRead(PINO_SENSOR_TENSAO);
   double valor_convertido = (valor_bruto * 5.0) / 1023.0 ;
   return valor_convertido;
 }
 
+
+/// @brief Realiza leitura de temperatura obtida pelo sensor
+/// @return Um double, com o valor da temperatura (em graus Celsuis)
 double leitura_temperatura(){
   int valor_bruto = analogRead(PINO_SENSOR_TEMPERATURA);
   double valor_convertido = 100 * (valor_bruto * 5.0) / 1023.0 ;
   return valor_convertido;
 }
 
+
+/// @brief Realiza leitura de corrente obtida pelo sensor
+/// @return Um double, com o valor da corrente (em Amperes)
 double leitura_corrente(){
   double valor_bruto = analogRead(PINO_SENSOR_CORRENTE);
   double valor_convertido = 10 * (((valor_bruto * 5.0) / 1023.0) - 2.5);
   return valor_convertido;
 }
 
+
+/// @brief Função executada quando a interrupção é acionada.
+void ISR_Sensores(){
+  digitalWrite(PINO_LED, digitalRead(PINO_INTERRUPT_SENSORES));
+}
+
+
+/// @brief Mostra os dados obtidos no display LCD.
 void mostra_dados(){
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -89,6 +135,8 @@ void mostra_dados(){
   }
 }
 
+
+/// @brief Envia os dados obtidos dos sensores por serial
 void envia_dados(){
   Serial.print(tensao);  
   Serial.print(", ");
@@ -97,25 +145,4 @@ void envia_dados(){
   Serial.print(temperatura);
   Serial.print(", ");
   Serial.println(estado_foto);
-}
-
-
-void setup() {
-  pinMode(PINO_LED, OUTPUT);
-  pinMode(PINO_FOTOSENSOR, INPUT);
-  pinMode(PINO_INTERRUPT_SENSORES, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PINO_INTERRUPT_SENSORES), ISR_Sensores, CHANGE);
-  Serial.begin(9600);
-  lcd.begin(16, 4);
-}
-
-void loop(){
-  tensao = leitura_tensao();
-  temperatura = leitura_temperatura();
-  corrente = leitura_corrente();
-  estado_foto = !digitalRead(PINO_FOTOSENSOR);
-
-  mostra_dados();
-  envia_dados();
-  delay(3000);
 }
