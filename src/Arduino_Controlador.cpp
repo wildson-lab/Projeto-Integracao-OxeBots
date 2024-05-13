b4c48218b8389f90dd5875097876f21331220fb4
Wildson <79426222+wildson-lab@users.noreply.github.com>
2024-05-12 23:15:04 -0300

    Implementadas leituras de corrente, tensão e temperaturas

--- src/Arduino_Controlador.cpp
+++ src/Arduino_Controlador.cpp
@@ -8,6 +8,9 @@ e envio dos dados para um Concentrador.
 #include <Arduino.h>
 
 #define PINO_LED 13
+#define PINO_SENSOR_TENSAO A0
+#define PINO_SENSOR_TEMPERATURA A1
+#define PINO_SENSOR_CORRENTE A2
 #define PINO_INTERRUPT_FOTO 2
 #define PINO_INTERRUPT_SENSORES 3
 
@@ -24,7 +27,28 @@ void ISR_Sensores(){
   digitalWrite(PINO_LED, digitalRead(PINO_INTERRUPT_SENSORES));
 }
 
+
+double leitura_tensao(){
+  double valor_bruto = analogRead(PINO_SENSOR_TENSAO);
+  double valor_convertido = (valor_bruto * 5.0) / 1023.0 ;
+  return valor_convertido;
+}
+
+double leitura_temperatura(){
+  int valor_bruto = analogRead(PINO_SENSOR_TEMPERATURA);
+  double valor_convertido = 100 * (valor_bruto * 5.0) / 1023.0 ;
+  return valor_convertido;
+}
+
+double leitura_corrente(){
+  double valor_bruto = analogRead(PINO_SENSOR_CORRENTE);
+  double valor_convertido = 10 * (((valor_bruto * 5.0) / 1023.0) - 2.5);
+  return valor_convertido;
+}
+
+
 void setup() {
+  Serial.begin(9600);
   pinMode(PINO_LED, OUTPUT);
   pinMode(PINO_INTERRUPT_FOTO, INPUT);
   pinMode(PINO_INTERRUPT_SENSORES, INPUT);
@@ -32,5 +56,16 @@ void setup() {
 }
 
 void loop(){
+  tensao = leitura_tensao();
+  temperatura = leitura_temperatura();
+  corrente = leitura_corrente();
 
+  Serial.print(tensao);  
+  Serial.print(", ");
+  Serial.print(corrente);
+  Serial.print(", ");
+  Serial.print(temperatura);
+  Serial.print(", ");
+  Serial.println(estado_rele);
+  delay(1000);
 }
