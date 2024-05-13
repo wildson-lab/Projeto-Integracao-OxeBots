90355a526238e27f7f1c6dc6af4ec7db56694ce8
Wildson <79426222+wildson-lab@users.noreply.github.com>
2024-05-12 22:54:29 -0300

    incluída rotina de interrupção

--- src/Arduino_Controlador.cpp
+++ src/Arduino_Controlador.cpp
@@ -8,14 +8,29 @@ e envio dos dados para um Concentrador.
 #include <Arduino.h>
 
 #define PINO_LED 13
+#define PINO_INTERRUPT_FOTO 2
+#define PINO_INTERRUPT_SENSORES 3
+
+volatile bool acionado_interrupt_foto = false;
+volatile bool acionado_interrupt_sensor = false;
+
+double tensao;
+double corrente;
+double temperatura;
+bool estado_rele;
+
+
+void ISR_Sensores(){
+  digitalWrite(PINO_LED, digitalRead(PINO_INTERRUPT_SENSORES));
+}
 
 void setup() {
   pinMode(PINO_LED, OUTPUT);
+  pinMode(PINO_INTERRUPT_FOTO, INPUT);
+  pinMode(PINO_INTERRUPT_SENSORES, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PINO_INTERRUPT_SENSORES), ISR_Sensores, CHANGE);
 }
 
-void loop() {
-  digitalWrite(PINO_LED, HIGH);
-  delay(1000);
-  digitalWrite(PINO_LED, LOW);
-  delay(1000);
+void loop(){
+
 }
